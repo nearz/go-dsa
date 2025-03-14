b61c8db09984777c851a978194139009a629bbeb
nearz <andrewjneary@gmail.com>
2025-03-14 17:32:14 -0400

    quicksort

--- arrays/sort/quicksort.go
+++ arrays/sort/quicksort.go
@@ -1,7 +1,44 @@
 package sort
 
-func QuickSort(ints []int) {
+import (
+	"math/rand"
+	"time"
+)
+
+func QuickSort(arr []int) {
+	hi := len(arr) - 1
+	lo := 0
+	shuffle(arr)
+	quickSort(arr, lo, hi)
 }
 
-func quickSort() {
+func quickSort(arr []int, lo, hi int) {
+	if lo < hi {
+		pivIdx := partition(arr, hi, lo)
+		quickSort(arr, lo, pivIdx-1)
+		quickSort(arr, pivIdx+1, hi)
+	}
+}
+
+func partition(arr []int, hi, lo int) int {
+	piv := arr[hi]
+	i := lo
+	for j := lo; j < hi; j++ {
+		if arr[j] <= piv {
+			arr[i], arr[j] = arr[j], arr[i]
+			i++
+		}
+	}
+	arr[i], arr[hi] = arr[hi], arr[i]
+	return i
+}
+
+func shuffle(arr []int) {
+	src := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(src)
+	n := len(arr)
+	for i := n - 1; i > 0; i-- {
+		j := r.Intn(i + 1)
+		arr[i], arr[j] = arr[j], arr[i]
+	}
 }
--- arrays/sort/quicksort_test.go
+++ arrays/sort/quicksort_test.go
@@ -1,1 +1,29 @@
 package sort
+
+import (
+	"slices"
+	"testing"
+)
+
+func TestQuickSort(t *testing.T) {
+	ts := []int{0, 1, 2, 3, 4, 6, 7, 8}
+	s := []int{8, 2, 6, 4, 0, 1, 7, 3}
+	QuickSort(s)
+	if !slices.Equal(ts, s) {
+		t.Error("Slice not sorted correctly, Test 1")
+	}
+
+	ts = []int{5, 8, 14, 19, 24, 25, 31, 49, 50, 51, 57, 65}
+	s = []int{49, 5, 57, 14, 24, 8, 31, 65, 51, 25, 19, 50}
+	QuickSort(s)
+	if !slices.Equal(ts, s) {
+		t.Error("Slice not sorted correctly, Test 2")
+	}
+}
+
+// func TestShuffle(t *testing.T) {
+// 	ts := []int{5, 8, 14, 19, 24, 25, 31, 49, 50, 51, 57, 65}
+// 	shuffle(ts)
+// 	shuffle(ts)
+// 	fmt.Println(ts)
+// }
